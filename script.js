a64e71fefcfe752e4a3cc4cf65b7b5cd72020f29
kebsixx <rizieqanw@gmail.com>
2024-03-11 10:28:31 +0700

    add popover menfess

--- script.js
+++ script.js
@@ -88,3 +88,9 @@ backBtn.addEventListener("click", function (event) {
   displayMembers(members); // Tampilkan kembali semua anggota
   this.style.display = "none"; // Sembunyikan tombol "Back" setelah diklik
 });
+
+// Inisialisasi popover
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl);
+});
